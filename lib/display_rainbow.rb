bcc2915499f4b2107ba8f20c77d3979fdae6e983
keisha coleman <keishacoleman710@gmail.com>
2020-09-12 23:55:12 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,3 @@
+display_rainbow = (['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']);
 
-def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-  puts "R: #{red}, O: #{ornage}, Y: #{yellow}, G: #{green}, B: #{blue}, I: #{indigo}, V: #{violet}"
-end
-
-display_rainbow([0])
+d
